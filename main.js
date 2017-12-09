912e1a1de7cd9fef5137ecd65955aeecc9c7357d
Sean Stephenson <Sean.M.Stephenson@espn.com>
2017-12-09 13:44:29 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const SHA256 = require('crypto-js/sha256')
+
+class Block {
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index
+    this.timestamp = timestamp
+    this.data = data
+    this.previousHash = previousHash
+    this.hash = this.calculateHash()
+  }
+
+  calculateHash() {
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()]
+  }
+
+  createGenesisBlock() {
+    return new Block(0, '01/01/2017', 'Genesis Block', '0')
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1]
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash
+    newBlock.hash = newBlock.calculateHash()
+    this.chain.push(newBlock)
+  }
+}
+
+const soloCoin = new Blockchain()
+soloCoin.addBlock(new Block(1, '2017-05-05', { amount: 42 }))
+soloCoin.addBlock(new Block(2, '2017-07-04', { amount: 17 }))
+
+console.log(JSON.stringify(soloCoin, null, 4))
